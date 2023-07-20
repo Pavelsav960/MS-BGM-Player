a4c92c0dc3a6dc3052949ed8c1ce1d44150aaf94
Pavel Savelev <Pavelsav960@gmail.com>
2023-07-20 19:02:04 +0300

    liked songs added
    added functionality to like songs and add them to a new section. all seems to work ass expected.

--- timer.js
+++ timer.js
@@ -1,7 +1,7 @@
 // Constants and Variables Declaration
 const mapToSongs = {
   'map1': [
-    'assets/audio/elliniaSound/Missing You.mp3/audio/Go Picnic.mp3',
+    'assets/audio/elliniaSound/Missing You.mp3',
     'assets/audio/elliniaSound/Moonlight Shadow.mp3',
     'assets/audio/elliniaSound/When the Morning Comes.mp3'],
   'map2': [
@@ -28,31 +28,28 @@ const mapToSongs = {
     'assets/audio/ludiSound/Waltz For Work.mp3'
   ],
 };
-const hoursInput = document.getElementById('hoursInput');
-const minutesInput = document.getElementById('minutesInput');
-const secondsInput = document.getElementById('secondsInput');
-const songSelect = document.getElementById('songSelect');
-const startButton = document.getElementById('startButton');
-const resetButton = document.getElementById('resetButton');
-const countdownDisplay = document.getElementById('countdown');
-const body = document.querySelector('body');
+
 const mapSelect = document.getElementById('mapSelect');
-let countdownInterval;
-let alarmSound = null;
-let isPaused = false;
-let endTime = null;
-let remainingTime = null;
+const songSelect = document.getElementById('songSelect');
+const songNameDisplay = document.getElementById('currentSong');
+const playPauseButton = document.getElementById('playPauseButton');
+const playPauseIcon = document.getElementById('playPauseIcon');
+const prevButton = document.getElementById('prevButton');
+const nextButton = document.getElementById('nextButton');
+const audioElement = document.querySelector('audio');
+const volumeSlider = document.getElementById('volumeSlider');
+const songProgressBar = document.getElementById('songProgress');
+const currentTimeElement = document.getElementById('currentTime');
+const likeButton = document.getElementById('likeButton');
+
+const likedSongs = JSON.parse(localStorage.getItem('likedSongs')) || [];
+
+const body = document.querySelector('body');
+
+let currentSongIndex = 0;
+
 
 // Function Definitions
-function formatTime(totalSeconds) {
-  const hours = Math.floor(totalSeconds / 3600);
-  totalSeconds %= 3600;
-  const minutes = Math.floor(totalSeconds / 60);
-  const seconds = totalSeconds % 60;
-
-  return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
-}
-
 function changeBackground(map) {
   switch (map) {
     case 'map1':
@@ -74,87 +71,196 @@ function changeBackground(map) {
       body.style.backgroundImage = 'url("assets/images/ludiriumBG.jpg")';
       console.log('imin');
       break;
+    case 'likedSongs':
+      body.style.backgroundImage = 'url("assets/images/likedSongsBG.jpg")';
+      break;
     default:
       body.style.backgroundImage = 'none';
   }
   updateSongList(map);
+  audioElement.pause();
+  playPauseIcon.classList.remove('fa-pause');
+  playPauseIcon.classList.add('fa-play');
+  songProgressBar.value = 0; // Reset the progress bar
+  
 }
 
+
 function updateSongList(map) {
-  const songList = mapToSongs[map] || [];
-  songSelect.innerHTML = songList.map(songPath => {
+  const songList = map === 'likedSongs' ? likedSongs : mapToSongs[map] || [];
+  currentSongIndex = 0;  // Reset current song index to 0 when updating song list
+  songSelect.innerHTML = songList.map((songPath, index) => {
     // Get the song name by splitting the path by '/' and taking the last part, then removing the '.mp3' extension
     const songName = songPath.split('/').pop().replace('.mp3', '');
-    return `<option value="${songPath}">${songName}</option>`;
+    return `<option value="${songPath}" ${index === 0 ? 'selected' : ''}>${songName}</option>`;
   }).join('');
+  
+  if (songList.length > 0) {
+    audioElement.src = songList[0];  // Set the audio source to the first song in the list
+    songNameDisplay.textContent = 'Currently Playing: ' + songSelect.options[songSelect.selectedIndex].text;
+
+    if (likedSongs.includes(songSelect.value)) {
+      likeButton.classList.add('liked');
+    } else {
+      likeButton.classList.remove('liked');
+    }
+  } else {
+    audioElement.src = '';
+    songNameDisplay.textContent = 'No song is selected';
+  }
+}
+
+
+
+function togglePlayPause(forcePlay = false) {
+  console.log('togglePlayPause called, forcePlay:', forcePlay, 'audioElement.paused:', audioElement.paused);
+  if (audioElement.paused || forcePlay) {
+    audioElement.play().then(() => {
+      playPauseIcon.classList.remove('fa-play');
+      playPauseIcon.classList.add('fa-pause');
+    }).catch((error) => {
+      console.error('Error playing audio:', error);
+    });
+  } else {
+    audioElement.pause();
+    playPauseIcon.classList.remove('fa-pause');
+    playPauseIcon.classList.add('fa-play');
+  }
+}
+
+
+function prevSong() {
+  if (currentSongIndex > 0) {
+    currentSongIndex--;
+  } else {
+    currentSongIndex = songSelect.options.length - 1;  // Loop back to the last song if we're currently at the first song
+  }
+  songSelect.selectedIndex = currentSongIndex;
+  audioElement.src = songSelect.value;
+  songNameDisplay.textContent = 'Currently Playing: ' + songSelect.options[songSelect.selectedIndex].text;
+
+  if (likedSongs.includes(songSelect.value)) {
+    likeButton.classList.add('liked');
+  } else {
+    likeButton.classList.remove('liked');
+  }
+
+  togglePlayPause(true);  // Automatically play the new song
+}
+
+function nextSong() {
+  if (currentSongIndex < songSelect.options.length - 1) {
+    currentSongIndex++;
+  } else {
+    currentSongIndex = 0;  // Loop back to the first song if we're currently at the last song
+  }
+  songSelect.selectedIndex = currentSongIndex;
+  audioElement.src = songSelect.value;
+  songNameDisplay.textContent = 'Currently Playing: ' + songSelect.options[songSelect.selectedIndex].text;
+
+  if (likedSongs.includes(songSelect.value)) {
+    likeButton.classList.add('liked');
+  } else {
+    likeButton.classList.remove('liked');
+  }
+
+  togglePlayPause(true);  // Automatically play the new song
+}
+
+function formatTime(seconds) {
+  const minutes = Math.floor(seconds / 60);
+  seconds = Math.floor(seconds % 60);
+  return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+}
+
+function onSongSelectChange() {
+  currentSongIndex = songSelect.selectedIndex; // Update the current song index
+  audioElement.src = songSelect.value; // Update the source of the audio element to the selected song
+  songNameDisplay.textContent = 'Currently Playing: ' + songSelect.options[songSelect.selectedIndex].text;
+  if (likedSongs.includes(songSelect.value)) {
+    likeButton.classList.add('liked');
+  } else {
+    likeButton.classList.remove('liked');
+  }
+  togglePlayPause(true);  // Automatically play the new song
+}
+
+
+function likeSong() {
+  const selectedSong = songSelect.value;
+  if (!likedSongs.includes(selectedSong)) {
+    likedSongs.push(selectedSong);
+    likeButton.classList.add('liked');
+    localStorage.setItem('likedSongs', JSON.stringify(likedSongs));
+    alert('Added to liked songs!');
+  } else {
+    const songIndex = likedSongs.indexOf(selectedSong);
+    likedSongs.splice(songIndex, 1);
+    likeButton.classList.remove('liked');
+    localStorage.setItem('likedSongs', JSON.stringify(likedSongs));
+    alert('Removed from liked songs!');
+
+    // If we're in the liked songs playlist, refresh the song list
+    if (mapSelect.value === 'likedSongs') {
+      updateSongList('likedSongs');
+
+      // After updating the song list, pause the song, reset progress bar, and change play/pause button to "play"
+      audioElement.pause();
+      songProgressBar.value = 0;
+      playPauseIcon.classList.remove('fa-pause');
+      playPauseIcon.classList.add('fa-play');
+    }
+  }
 }
 
 
 
 // Event Listeners
-startButton.addEventListener('click', () => {
-  // If a timer is already running, stop it.
-  if (startButton.textContent === 'Stop') {
-    clearInterval(countdownInterval);
-    remainingTime = endTime - Date.now();
-    startButton.textContent = 'Start';
-    isPaused = true;
-    return;
-  }
-  let totalSeconds;
-  if (!isPaused) {
-    const hours = parseInt(hoursInput.value, 10) || 0;
-    const minutes = parseInt(minutesInput.value, 10) || 0;
-    const seconds = parseInt(secondsInput.value, 10) || 0;
-    totalSeconds = hours * 3600 + minutes * 60 + seconds;
-    if (totalSeconds <= 0) {
-      alert('Please enter a valid time.');
-      return;
-    }
-    endTime = Date.now() + totalSeconds * 1000;
-    countdownDisplay.textContent = formatTime(totalSeconds);
-    alarmSound = new Audio(songSelect.value);
-  } else {
-    totalSeconds = Math.round(remainingTime / 1000);
-    endTime = Date.now() + remainingTime;
-    isPaused = false;
-  }
-  startButton.textContent = 'Stop';
-  countdownInterval = setInterval(() => {
-    const remainingSeconds = Math.round((endTime - Date.now()) / 1000);
-    countdownDisplay.textContent = formatTime(remainingSeconds);
-    if (remainingSeconds <= 0) {
-      clearInterval(countdownInterval);
-      if (alarmSound) {
-        alarmSound.play();
-      }
-      alert('Time is up!');
-      startButton.textContent = 'Start';
-    }
-  }, 1000);
-});
-
-resetButton.addEventListener('click', () => {
-  if (alarmSound) {
-    alarmSound.pause();
-    alarmSound.currentTime = 0;
-  }
-  clearInterval(countdownInterval);
-  countdownDisplay.textContent = '00:00:00';
-  hoursInput.value = null;
-  minutesInput.value = null;
-  secondsInput.value = null;
-  endTime = null;
-  isPaused = false;
-  remainingTime = null;
-  startButton.textContent = 'Start';
-});
-
 mapSelect.addEventListener('change', () => {
   const selectedMap = mapSelect.value;
   changeBackground(selectedMap);
+  updateSongList(selectedMap);
   localStorage.setItem('selectedMap', selectedMap); // Save selected map to localStorage
 });
+
+volumeSlider.addEventListener('input', function() {
+  audioElement.volume = volumeSlider.value;
+});
+
+audioElement.addEventListener('timeupdate', () => {
+  if (isFinite(audioElement.duration)) {
+    const progressPercent = (audioElement.currentTime / audioElement.duration) * 100;
+    songProgressBar.value = progressPercent;
+  }
+});
+
+
+songProgressBar.addEventListener('click', function(e) {
+  const totalWidth = this.offsetWidth;
+  const clickLocation = e.offsetX;
+  const selectedPercent = (clickLocation / totalWidth);
+  audioElement.currentTime = selectedPercent * audioElement.duration;
+});
+
+audioElement.addEventListener('timeupdate', () => {
+    if (isFinite(audioElement.duration)) {
+        const progressPercent = (audioElement.currentTime / audioElement.duration) * 100;
+        songProgressBar.value = progressPercent;
+
+        const currentTime = audioElement.currentTime;
+        currentTimeElement.textContent = formatTime(currentTime); // Change this line
+    }
+});
+
+
+
+playPauseButton.addEventListener('click', () => togglePlayPause());
+prevButton.addEventListener('click', prevSong);
+nextButton.addEventListener('click', nextSong);
+audioElement.addEventListener('ended', nextSong);
+songSelect.addEventListener('change', onSongSelectChange);
+likeButton.addEventListener('click', likeSong);
+
 
 // Executable code
 const savedMap = localStorage.getItem('selectedMap');
@@ -162,4 +268,6 @@ if (savedMap) {
   mapSelect.value = savedMap;
   changeBackground(savedMap);
   updateSongList(savedMap);
+} else {
+  updateSongList('map1');  // Default to 'map1' if there's no saved map
 }
